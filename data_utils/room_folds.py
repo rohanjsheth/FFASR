477720abf9d8bcbf2e1123de767178c7b87b4a84
Rohan Sheth <rohanjaiminsheth@gmail.com>
2026-08-19 02:11:02 -0700

    Room mod

--- data_utils/room_folds.py
+++ data_utils/room_folds.py
@@ -1,19 +1,23 @@
+# Sets are matched on C50 (9.00 dB both) and T30 (0.506 / 0.503 s), not just on
+# room type. Splitting by type alone put the more reverberant room of four out of
+# five pairs in the same set.
+
 ROOMS_A: frozenset[str] = frozenset(
     {
         "Room9",  # bathroom1
         "Room8",  # bedroom2
-        "Room3",  # living_room_w_hallway1
         "Room2",  # living_room2
-        "Room6",  # meeting_room2
+        "Room4",  # living_room_w_hallway2
+        "Room5",  # meeting_room1
     }
 )
 ROOMS_B: frozenset[str] = frozenset(
     {
         "Room10",  # bathroom2
-        "Room7",  # bedroom1
-        "Room4",  # living_room_w_hallway2
-        "Room1",  # living_room1
-        "Room5",  # meeting_room1
+        "Room7",   # bedroom1
+        "Room1",   # living_room1
+        "Room3",   # living_room_w_hallway1
+        "Room6",   # meeting_room2
     }
 )
 
